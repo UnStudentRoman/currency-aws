9bf774a8abba0ec31aac0ff22475693ea0d6be1a
Marius Cristian <marius.cristian@lseg.com>
2024-03-12 11:39:15 +0200

    Add variables file

--- vars.py
+++ vars.py
@@ -1,6 +1,7 @@
 base_url = "https://cdn.jsdelivr.net"
 api_endpoint = "/npm/@fawazahmed0/currency-api"
 
+table_name = 'Currency'
 base_curr = 'ron'
-
+region_name='eu-north-1'
 curr_exchange = ['usd', 'eur', 'mdl', 'btc', 'jpy', 'pln', 'gbp']
